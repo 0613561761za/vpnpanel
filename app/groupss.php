7665d2da73ee13dc416e513b97509171fd5c0610
rizalio <admin@rizalfakhri.com>
2017-02-06 20:04:01 +0700

    djjdj

--- app/groupss.php
+++ app/groupss.php
@@ -5,14 +5,17 @@ use Group;
 
 class groupss
 {
-    $groups = Group::get();
-    if($groups->count() < 1)
+    public function groups()
     {
-      return "No Groups Found"
-    }
+      $groups = Group::get();
+      if($groups->count() < 1)
+      {
+        return "No Groups Found"
+      }
 
-    foreach($groups as $group)
-    {
-      echo "Select Server " . $group->group_country;
+      foreach($groups as $group)
+      {
+        echo "Select Server " . $group->group_country;
+      }
     }
 }
